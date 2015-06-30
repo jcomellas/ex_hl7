9ebb613895bd5a84c05f5dc6ae2de535f9e9f1ce
Juan Jose Comellas <juanjo@comellas.org>
2015-06-30 15:43:54 -0300

    Fix error in type specs detected by Elixir 1.0.5

--- lib/ex_hl7/type.ex
+++ lib/ex_hl7/type.ex
@@ -3,8 +3,8 @@ defmodule HL7.Type do
   @type sequence       :: pos_integer
   @type composite_id   :: binary
   @type item_type      :: :field | :component | :repetition | :subcomponent
-  @type subcomponent   :: binary | tuple(binary)
-  @type component      :: binary | tuple(binary) | tuple(subcomponent)
+  @type subcomponent   :: binary | tuple
+  @type component      :: binary | tuple
   @type field          :: binary | component | [binary] | [component]
   @type value_type     :: :string | :integer | :float | :date | :datetime
   @type value          :: binary | integer | float | :calendar.date | :calendar.datetime
