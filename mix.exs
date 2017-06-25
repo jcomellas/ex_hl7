1ff3e37ba8dac3b419545c8eb0b6cf6ef50a44a7
Juan Jose Comellas <juanjo@comellas.org>
2017-06-25 18:45:11 -0300

    Update dependencies

--- mix.exs
+++ mix.exs
@@ -13,24 +13,12 @@ defmodule HL7.Mixfile do
      consolidate_protocols: Mix.env != :test]
   end
 
-  # Configuration for the OTP application
-  #
-  # Type `mix help compile.app` for more information
   def application do
     [applications: [:logger]]
   end
 
-  # Dependencies can be Hex packages:
-  #
-  #   {:mydep, "~> 0.3.0"}
-  #
-  # Or git/path repositories:
-  #
-  #   {:mydep, git: "https://github.com/elixir-lang/mydep.git", tag: "0.1.0"}
-  #
-  # Type `mix help deps` for more examples and options
   defp deps do
-    [{:ex_doc, "~> 0.11", only: :dev}]
+    [{:ex_doc, "~> 0.16.1", only: :dev}]
   end
 
   defp package do
