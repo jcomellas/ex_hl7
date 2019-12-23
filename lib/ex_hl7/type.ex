e745c74ac12215b690eb3d284d5bf3df1fc7c0d3
Juan Jose Comellas <juanjo@comellas.org>
2019-12-23 12:19:44 -0300

    Cleanup global type specs

--- lib/ex_hl7/type.ex
+++ lib/ex_hl7/type.ex
@@ -2,14 +2,14 @@ defmodule HL7.Type do
   @moduledoc """
   Type specifications used by the library.
   """
-  @type segment_id     :: binary
-  @type sequence       :: pos_integer
-  @type repetition     :: pos_integer
-  @type composite_id   :: binary
-  @type item_type      :: :field | :component | :repetition | :subcomponent
-  @type subcomponent   :: binary | tuple
-  @type component      :: binary | tuple
-  @type field          :: binary | component | [binary] | [component]
-  @type value_type     :: :string | :integer | :float | :date | :datetime
-  @type value          :: binary | integer | float | Date.t | NaiveDateTime.t
+  @type segment_id :: binary
+  @type sequence :: pos_integer
+  @type repetition :: pos_integer
+  @type composite_id :: binary
+  @type item_type :: :field | :component | :repetition | :subcomponent
+  @type subcomponent :: binary | tuple
+  @type component :: binary | tuple
+  @type field :: binary | component | [binary] | [component]
+  @type value_type :: :string | :integer | :float | :date | :datetime
+  @type value :: binary | integer | float | Date.t() | NaiveDateTime.t()
 end
