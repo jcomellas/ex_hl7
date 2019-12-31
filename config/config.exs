fde7b423ece923aa291f4e6ac5fc147317a67e71
Juan Jose Comellas <juanjo@comellas.org>
2019-12-31 07:45:55 -0300

    Clean up configuration

--- config/config.exs
+++ config/config.exs
@@ -1,24 +1,14 @@
-# This file is responsible for configuring your application
-# and its dependencies with the aid of the Mix.Config module.
 use Mix.Config
 
-# This configuration is loaded before any dependency and is restricted
-# to this project. If another project depends on this project, this
-# file won't be loaded nor affect the parent project. For this reason,
-# if you want to provide default values for your application for third-
-# party users, it should be done in your mix.exs file.
-
-# Sample configuration:
-#
-#     config :logger, :console,
-#       level: :info,
-#       format: "$date $time [$level] $metadata$message\n",
-#       metadata: [:user_id]
-
-# It is also possible to import configuration files, relative to this
-# directory. For example, you can emulate configuration per environment
-# by uncommenting the line below and defining dev.exs, test.exs and such.
-# Configuration from the imported file will override the ones defined
-# here (which is why it is important to import them last).
-#
-#     import_config "#{Mix.env}.exs"
+config :ex_hl7,
+  # Default HL7 message field separator (MSH.1)
+  field_sep: "|",
+  # Default HL7 encoding characters (i.e. separators for components, repetitions, subcomponents
+  # and escaping) (MSH.2)
+  encoding_chars: "^~\\&",
+  # Default HL7 processing ID (MSH.11)
+  processing_id: "P",
+  # Default HL7 version (MSH.12)
+  version: "2.4",
+  # Default country code (MSH.17)
+  country_code: "ARG"
